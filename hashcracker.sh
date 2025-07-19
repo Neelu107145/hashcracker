463bd16f99c42da138799009b3c9f3766e2b08ec
Neel Suhagiya <neelsuvagiya0810@gmail.com>
2025-07-19 09:14:02 +0530

    Update hashcracker.sh

--- hashcracker.sh
+++ hashcracker.sh
@@ -6,14 +6,14 @@ echo $val | cat > file1.txt
 read -p "Which file do you want to use default/custom (de/cu) :" file
 
 if [ $file = "de" ] ; then
-        john --format=$hashtype --wordlist=/home/neellinux/WORDLIST/rockyou.txt file1.txt
+        john --format=$hashtype --wordlist=/usr/share/wordlists/rockyou.txt file1.txt
 else
 	read -p "Do you want to set the parameter of custom file (y/n) :" ans
 	if [ $ans = "y" ] ; then
-		bash /home/neellinux/WORDLIST/replace.sh
-	        john --format=$hashtype --wordlist=/home/neellinux/WORDLIST/demo.txt file1.txt
+		bash replace.sh
+	        john --format=$hashtype --wordlist=user_data_wordlist.txt file1.txt
 	else
-		john --format=$hashtype --wordlist=/home/neellinux/WORDLIST/demo.txt file1.txt
+		john --format=$hashtype --wordlist=user_data_wordlist.txt file1.txt
 	fi	
 
 fi
