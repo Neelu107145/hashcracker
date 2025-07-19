ed4c569465e54f0e0cf466dedc4d7a88ec4016f5
Neel Suhagiya <neelsuvagiya0810@gmail.com>
2025-07-19 09:42:58 +0530

    load the user data in wordlist

--- replace.sh
+++ replace.sh
@@ -4,7 +4,7 @@ read -p "What do you want Replace/Reset (rp/rs): " answer
 
 if [ $answer = "rp" ] ; then
 
-        file="demo.txt"
+        file="user_data_wordlist.txt"
         read -p "Enter the name (First letter should be capital): " Name
         name=${Name,,}
         NAME=${Name^^}
@@ -13,17 +13,17 @@ if [ $answer = "rp" ] ; then
         read -p "Enter the birthdate (dd/mm/yyyy): " wdob
         read -p "Enter the birthdate (dd/mm/yy): " dob
         read -p "Enter the phone number: " pno
-        sed -i "s/"Neel"/$Name/g" $file
-        sed -i "s/"neel"/$name/g" $file
-        sed -i "s/"NEEL"/$NAME/g" $file
-        sed -i "s/"suvagiya"/$surname/g" $file
-        sed -i "s/"SUVAGIYA"/$SURNAME/g" $file
-        sed -i "s/"08102005"/$wdob/g" $file
-        sed -i "s/"081005"/$dob/g" $file
-        sed -i "s/"7043945200"/$pno/g" $file
+        sed -i "s/"Bob"/$Name/g" $file
+        sed -i "s/"bob"/$name/g" $file
+        sed -i "s/"BOB"/$NAME/g" $file
+        sed -i "s/"anderson"/$surname/g" $file
+        sed -i "s/"ANDERSON"/$SURNAME/g" $file
+        sed -i "s/"06062005"/$wdob/g" $file
+        sed -i "s/"060605"/$dob/g" $file
+        sed -i "s/"9635512343"/$pno/g" $file
 else
-        rm demo.txt
-        cp password.txt demo.txt
+        rm user_data_wordlist.txt
+        cp user_duplicate.txt user_data_wordlist.txt
 fi      
 
 
